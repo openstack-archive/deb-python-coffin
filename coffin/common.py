5abef3176c76ec8c0c7bbd70a187e3e99555cff6
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-10-07 02:14:16 +0200

    allow a setting to load custom filters

--- coffin/common.py
+++ coffin/common.py
@@ -36,43 +36,59 @@ def _get_loaders():
 
 
 def _get_filters():
-    """Provides universally used filters; e.g. the `url` function
-    that ties into Django's URLConf.
+    """Returns a list of filters to provide through Jinja. This includes
+    ported versions of Django's builtin filters that Jinja is lacking,
+    as well as custom filters as specified by the user in the settings.
 
     :return: A mapping of names to filters.
-
-    # TODO: Most of those need to updated for autoescaping
     """
-    def url(view_name, *args, **kwargs):
-        from django.core.urlresolvers import reverse, NoReverseMatch
-        url = reverse(view_name, args=args, kwargs=kwargs)
-        return url
+    def get_django():
+        # TODO: Most of those need to updated for autoescaping
+        def url(view_name, *args, **kwargs):
+            from django.core.urlresolvers import reverse, NoReverseMatch
+            url = reverse(view_name, args=args, kwargs=kwargs)
+            return url
 
-    def timesince(value, arg=None):
-        from django.utils.timesince import timesince
-        if arg:
-            return timesince(value, arg)
-        return timesince(value)
+        def timesince(value, arg=None):
+            from django.utils.timesince import timesince
+            if arg:
+                return timesince(value, arg)
+            return timesince(value)
 
-    def timeuntil(value, arg=None):
-        from django.utils.timesince import timeuntil
-        return timeuntil(date, arg)
+        def timeuntil(value, arg=None):
+            from django.utils.timesince import timeuntil
+            return timeuntil(date, arg)
 
-    def date(value, arg=None):
-        from django.conf import settings
-        from django.utils.dateformat import format
-        if arg is None:
-            arg = settings.DATE_FORMAT
-        return format(value, arg)
+        def date(value, arg=None):
+            from django.conf import settings
+            from django.utils.dateformat import format
+            if arg is None:
+                arg = settings.DATE_FORMAT
+            return format(value, arg)
 
-    def time(value, arg=None):
-        from django.conf import settings
-        from django.utils.dateformat import time_format
-        if arg is None:
-            arg = settings.TIME_FORMAT
-        return time_format(value, arg)
+        def time(value, arg=None):
+            from django.conf import settings
+            from django.utils.dateformat import time_format
+            if arg is None:
+                arg = settings.TIME_FORMAT
+            return time_format(value, arg)
 
-    return locals()
+        return locals()
+
+    from django.conf import settings
+    from django.core.urlresolvers import get_callable
+
+    result = get_django()
+    user = getattr(settings, 'JINJA2_FILTERS', {})
+    if isinstance(user, dict):
+        for key, value in user.items():
+            result[user] = callable(value) and value or get_callable(value)
+    else:
+        for value in user:
+            value = callable(value) and value or get_callable(value)
+            result[value.__name__] = value
+
+    return result
 
 
 def _get_extensions():
