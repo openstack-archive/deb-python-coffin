d0f2ab2bd15aed52d244156ed83d5653a66d3151
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-11-11 01:48:44 +0100

    load extensions from templatetag libraries when setting up jinja2 environment

--- coffin/common.py
+++ coffin/common.py
@@ -1,3 +1,4 @@
+import os
 import warnings
 
 from jinja2 import Environment, loaders
@@ -10,6 +11,7 @@ __all__ = ('get_env', 'dict_from_django_context')
 
 _ENV = None
 _LOADERS = [] # See :fun:`_infer_loaders`.
+_TEMPLATE_LIBS = []
 _JINJA_I18N_EXTENSION_NAME = 'jinja2.ext.i18n'
 
 
@@ -33,6 +35,38 @@ def _get_loaders():
         else: # It's assumed to be a Jinja2 loader instance.
             _LOADERS.append(loader)
     return _LOADERS
+
+
+def _get_templatelibs():
+    """Return an iterable of template ``Library`` instances.
+
+    Since we cannot support the {% load %} tag in Jinja, we have to
+    register all libraries globally.
+    """
+    if _TEMPLATE_LIBS:
+        return _TEMPLATE_LIBS
+
+    from django.conf import settings
+    from django.template import get_library, InvalidTemplateLibrary
+
+    for a in settings.INSTALLED_APPS:
+        try:
+            path = __import__(a + '.templatetags', {}, {}, ['']).__file__
+            path = os.path.dirname(path)  # we now have the templatetags/ directory
+        except ImportError:
+            pass
+        else:
+            for f in os.listdir(path):
+                if f == '__init__.py':
+                    continue
+                if f.endswith('.py'):
+                    try:
+                        # TODO: will need updating when #6587 lands
+                        _TEMPLATE_LIBS.append(get_library(
+                            "django.templatetags.%s" % os.path.splitext(f)[0]))
+                    except InvalidTemplateLibrary:
+                        pass
+    return _TEMPLATE_LIBS
 
 
 def _get_filters():
@@ -76,26 +110,45 @@ def _get_filters():
         return locals()
 
     from django.conf import settings
-    from django.core.urlresolvers import get_callable
 
-    result = get_django()
+    # start with our default builtins
+    filters = get_django()
+
+    # add the globally defined filter list
     user = getattr(settings, 'JINJA2_FILTERS', {})
+    from django.core.urlresolvers import get_callable
     if isinstance(user, dict):
         for key, value in user.items():
-            result[user] = callable(value) and value or get_callable(value)
+            filters[user] = callable(value) and value or get_callable(value)
     else:
         for value in user:
             value = callable(value) and value or get_callable(value)
-            result[value.__name__] = value
+            filters[value.__name__] = value
 
-    return result
+    # add filters defined in application's templatetag librarie
+    for lib in _get_templatelibs():
+        if hasattr(lib, 'jinja2_filters'):
+            filters.update(lib.jinja2_filters)
+
+    return filters
 
 
 def _get_extensions():
     from django.conf import settings
-    extensions = list(getattr(settings, 'JINJA2_EXTENSIONS', []))
+
+    # start with our default builtins
+    extensions = ['coffin.template.defaulttags.LoadExtension']
     if settings.USE_I18N:
-        extensions.append(_JINJA_I18N_EXTENSION_NAME)      
+        extensions.append(_JINJA_I18N_EXTENSION_NAME)
+
+    # add the globally defined extension list
+    extensions += list(getattr(settings, 'JINJA2_EXTENSIONS', []))
+
+    # add extensions defined in application's templatetag libraries
+    for lib in _get_templatelibs():
+        if hasattr(lib, 'jinja2_extensions'):
+            extensions += lib.jinja2_extensions
+
     return extensions
 
 
