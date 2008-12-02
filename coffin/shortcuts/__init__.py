0a8e928f133814f50b478269586b863303e10894
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-12-02 09:57:53 +0100

    added jinja-replacements for the default 404/500 handlers

--- coffin/shortcuts/__init__.py
+++ coffin/shortcuts/__init__.py
@@ -5,9 +5,16 @@ from django.template.context import Context as DjangoContext
 from coffin.common import dict_from_django_context, get_env
 
 
-def render_to_response(template_name, dictionary=None, context_instance=None,
-                       mimetype=None):
+__all__ = ('render_template', 'render_to_response',)
+
+
+def render_template(template_name, dictionary=None, context_instance=None):
     """
+    # TODO: Not quite sure if this is the right approach - we need to
+    provide some generic way to render a Jinja template using a
+    RequestContext though, for instances where a full render_to_response
+    is not appropriate (e.g. custom HTTP Response class).
+
     :param template_name: Filename of the template to get or a sequence of
         filenames to try, in order.
     :param dictionary: Rendering context for the template.
@@ -20,5 +27,16 @@ def render_to_response(template_name, dictionary=None, context_instance=None,
     assert isinstance(dictionary, dict) # Required for **-operator.
     if context_instance:
         dictionary.update(dict_from_django_context(context_instance))
-    rendered = template.render(**dictionary)
+    return template.render(**dictionary)
+
+
+def render_to_response(template_name, dictionary=None, context_instance=None,
+                       mimetype=None):
+    """
+    :param template_name: Filename of the template to get or a sequence of
+        filenames to try, in order.
+    :param dictionary: Rendering context for the template.
+    :returns: A response object with the evaluated template as a payload.
+    """
+    rendered = render_template(template_name, dictionary, context_instance)
     return HttpResponse(rendered, mimetype=mimetype)
