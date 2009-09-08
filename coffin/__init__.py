7f166e12a29011a245a759be1c02efebe778cebe
David Cramer <dcramer@david-cramers-macbook.local>
2009-09-08 13:20:55 -0500

    updated coffin url

--- coffin/__init__.py
+++ coffin/__init__.py
@@ -2,7 +2,7 @@
 Coffin
 ~~~~~~
 
-`Coffin <https://launchpad.net/coffin/>` is a package that resolves the
+`Coffin <http://www.github.com/dcramer/coffin>` is a package that resolves the
 impedance mismatch between `Django <http://www.djangoproject.com/>` and `Jinja2
 <http://jinja.pocoo.org/2/>` through various adapters. The aim is to use Coffin
 as a drop-in replacement for Django's template system to whatever extent is
