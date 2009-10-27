160d547843f95e8b127ee7903863b51089b5678f
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-10-27 13:10:51 -0500

    timeuntil needs to pass the argument optionally

--- coffin/__init__.py
+++ coffin/__init__.py
@@ -14,7 +14,7 @@ reasonable.
 
 
 __all__ = ('__version__', '__build__', '__docformat__', 'get_revision')
-__version__ = (0, 3, 1)
+__version__ = (0, 3, 2)
 __docformat__ = 'restructuredtext en'
 
 import os
