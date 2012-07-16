295c076e6b5f3ee09498e2b027c332e7148e43b6
dpantele <panteleev.do@gmail.com>
2012-07-16 16:55:53 +0400

    Fixed Copy-Paste typo

--- coffin/views/generic/detail.py
+++ coffin/views/generic/detail.py
@@ -1,7 +1,7 @@
 import django.views.generic.detail as _generic_detail
 from coffin.views.generic.base import TemplateResponseMixin as JinjaTemplateResponseMixin
 
-class SingleObjectTemplateResponseMixin(JinjaTemplateResponseMixin, _generic_detail.TemplateResponseMixin):
+class SingleObjectTemplateResponseMixin(JinjaTemplateResponseMixin, _generic_detail.SingleObjectTemplateResponseMixin):
     """
     Equivalent of django mixin SingleObjectTemplateResponseMixin, but uses Jinja template renderer.
     """
