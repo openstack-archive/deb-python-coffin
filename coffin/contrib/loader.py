ca6644c81bb0fc305113bbda0916a098ccf08703
Vladislav Poluhin <nuklea@gmail.com>
2012-11-13 10:31:24 +0800

    JINJA2_DEFAULT_TEMPLATE_EXTENSION now supports multiple values

--- coffin/contrib/loader.py
+++ coffin/contrib/loader.py
@@ -13,20 +13,26 @@ TEMPLATE_LOADERS = (
 
 """
 
+from os.path import splitext
 from coffin.common import env
 from django.conf import settings
 from django.template.loaders import app_directories, filesystem
 
 
 JINJA2_DEFAULT_TEMPLATE_EXTENSION = getattr(settings,
-    'JINJA2_DEFAULT_TEMPLATE_EXTENSION', '.jinja')
+    'JINJA2_DEFAULT_TEMPLATE_EXTENSION', ('.jinja',))
+
+if isinstance(JINJA2_DEFAULT_TEMPLATE_EXTENSION, basestring):
+    JINJA2_DEFAULT_TEMPLATE_EXTENSION = (JINJA2_DEFAULT_TEMPLATE_EXTENSION,)
 
 
 class LoaderMixin(object):
     is_usable = True
 
     def load_template(self, template_name, template_dirs=None):
-        if not template_name.endswith(JINJA2_DEFAULT_TEMPLATE_EXTENSION):
+        extension = splitext(template_name)[1]
+
+        if not extension in JINJA2_DEFAULT_TEMPLATE_EXTENSION:
             return super(LoaderMixin, self).load_template(template_name,
                 template_dirs)
         template = env.get_template(template_name)
