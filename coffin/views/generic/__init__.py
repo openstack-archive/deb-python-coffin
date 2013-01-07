25eb6593456ec7387de1ae65083cafa68794abb7
Davide Setti <davide.setti@gmail.com>
2013-01-07 12:30:05 +0100

    fixed a bug in generic views import

--- coffin/views/generic/__init__.py
+++ coffin/views/generic/__init__.py
@@ -1,1 +1,10 @@
-from django.views.generic import *
+from django.views.generic import GenericViewError
+from django.views.generic.base import View, RedirectView
+
+from coffin.views.generic.base import TemplateView
+from coffin.views.generic.dates import (ArchiveIndexView, YearArchiveView, MonthArchiveView,
+                                        WeekArchiveView, DayArchiveView, TodayArchiveView,
+                                        DateDetailView)
+from coffin.views.generic.detail import DetailView
+from coffin.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from coffin.views.generic.list import ListView
