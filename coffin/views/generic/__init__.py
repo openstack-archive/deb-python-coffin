ab02bdc3f32517c740c0c36f486fc3e8e8692be4
Frankie Dintino <fdintino@theatlantic.com>
2013-03-25 14:39:36 -0400

    Catch ImportError of Django 1.3+ class-based views for 1.2 compat

--- coffin/views/generic/__init__.py
+++ coffin/views/generic/__init__.py
@@ -1,10 +1,15 @@
 from django.views.generic import GenericViewError
-from django.views.generic.base import View, RedirectView
-
-from coffin.views.generic.base import TemplateView
-from coffin.views.generic.dates import (ArchiveIndexView, YearArchiveView, MonthArchiveView,
-                                        WeekArchiveView, DayArchiveView, TodayArchiveView,
-                                        DateDetailView)
-from coffin.views.generic.detail import DetailView
-from coffin.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
-from coffin.views.generic.list import ListView
+try:
+    from django.views.generic.base import View, RedirectView
+except ImportError:
+    pass
+else:
+    from coffin.views.generic.base import TemplateView
+    from coffin.views.generic.dates import (ArchiveIndexView, YearArchiveView,
+                                            MonthArchiveView, WeekArchiveView,
+                                            DayArchiveView, TodayArchiveView,
+                                            DateDetailView)
+    from coffin.views.generic.detail import DetailView
+    from coffin.views.generic.edit import (FormView, CreateView, UpdateView,
+                                           DeleteView)
+    from coffin.views.generic.list import ListView
