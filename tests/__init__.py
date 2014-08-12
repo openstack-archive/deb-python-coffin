3142f10373f360972ed63e91ffae369181636435
Thomas Goirand <thomas@goirand.fr>
2014-08-12 15:06:25 +0800

    Added initial debian folder.

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,11 @@ import sys
 # module code rather than setup_package(), because we want it to run
 # before any module-wide imports in any of the test modules.
 sys.path.insert(0, path.join(path.dirname(__file__), 'res', 'apps'))
-from django.core.management import setup_environ
-import settings
-setup_environ(settings)
+
+import os
+import django
+from templatelibs_app import models
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.res.apps.settings")
+django.setup()
+
