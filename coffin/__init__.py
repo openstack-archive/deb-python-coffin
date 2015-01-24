d89a5313e521251285947e72931746aaea3ff554
Michael Elsdörfer <michael@elsdoerfer.com>
2015-01-24 13:36:44 +0100

    Update version.

--- coffin/__init__.py
+++ coffin/__init__.py
@@ -14,7 +14,7 @@ reasonable.
 
 
 __all__ = ('__version__', '__build__', '__docformat__', 'get_revision')
-__version__ = (0, 3, '8')
+__version__ = (0, 4, '0')
 __docformat__ = 'restructuredtext en'
 
 import os
